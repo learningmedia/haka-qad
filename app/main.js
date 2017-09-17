2f18fb988ef7f19bc8045e68da5dadfaed8f15ff
Andreas Helmberger <public@ahelmberger.de>
2017-09-17 21:29:47 +0200

    Create haka-root component

--- app/main.js
+++ app/main.js
@@ -1,43 +1,13 @@
-import HelloWorld from './hello-world';
-
-const handlers = {
-  showQuestionLayer
-};
-
-function htmlEncode(text) {
-  const div = document.createElement('div');
-  div.textContent = text;
-  return div.innerHTML;
-}
-
-function resolveUrl(rootUrl, url) {
-  return `${rootUrl}${url}`;
-}
-
-function createHtml(config, rootUrl) {
-  return `
-    <div class="haka-root">
-      <video class="haka-video" src="${resolveUrl(rootUrl, config.video)}" controls></video>
-      <div class="haka-sidebar"></div>
-    </div>
-  `;
-}
+import HakaRoot from './haka-root';
 
 function renderProject(container, config, rootUrl) {
-  const markers = config.markers;
-  container.innerHTML = createHtml(config, rootUrl);
-  const video = container.querySelector('.haka-video');
 
-  video.addEventListener('timeupdate', () => {
-    const index = markers.findIndex(marker => video.currentTime >= marker.timecode);
-    if (index !== -1) {
-      const currentMarker = markers[index];
-      markers.splice(index, 1);
-      handlers[currentMarker.handler](container, currentMarker.params);
-    }
-  }, false);
+  const hakaApp = new HakaRoot({
+    target: document.getElementById('test'),
+    data: { config, rootUrl }
+  });
 
-  video.play();
+  hakaApp.start();
 }
 
 function startHaka(container, configUrl, rootUrl) {
@@ -49,30 +19,4 @@ function startHaka(container, configUrl, rootUrl) {
   .catch(error => alert(error));
 }
 
-  document.body.addEventListener('click', event => {
-
-  if (event.target.getAttribute('data-role') === 'answer') {
-    const textElement = event.target.parentNode.querySelector('[data-role="text"]');
-    textElement.textContent = event.target.getAttribute('data-text');
-    textElement.classList.remove('haka-inactive');
-  }
-
-});
-
-function showQuestionLayer(container, params) {
-  const markerHtml = `
-    <div>
-      <p>${htmlEncode(params.question)}</p>
-      ${params.answers.map(answer => `<button data-role="answer" data-text="${htmlEncode(answer.text)}">${htmlEncode(answer.value)}</button>`).join('')}
-      <p class="haka-inactive" data-role="text"></p>
-    </div>
-  `;
-  container.querySelector('.haka-sidebar').insertAdjacentHTML('beforeend', markerHtml);
-}
-
 $(() => startHaka(document.getElementById('main'), 'example/example.json', 'example/'));
-
-const app = new HelloWorld({
-  target: document.getElementById('test'),
-  data: { name: 'world' }
-});
